bb2dc707a653488cad6b9f1b9825702a7e527302
Trey Wood <edwinjwood3rd@gmail.com>
2016-10-31 12:26:40 -0500

    controller property and route action observable

--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -3,14 +3,19 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model() {
-    const pos = x => x * 10;
-    const neg = x => x * -10;
+    const pos = x => x * 1;
+    const neg = x => x * -1;
 
     return this.observable.action('reverse')
       .scan(op => op === pos ? neg : pos, pos)
       .startWith(pos)
       .combineLatest(Rx.Observable.interval(1000))
       .map(([op, n]) => op(n));
+  },
+
+  setupController(controller, model) {
+    let xs = model.combineLatest(controller.observable.property('multiplier')).map(([x,y]) => x * y);
+    this._super(controller, xs);
   },
 
   actions: {
